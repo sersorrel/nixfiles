c4756dd0d80832c969f1cb3c041d1969c559bb99
Ash Holland <ash@sorrel.sh>
2021-08-01 16:45:24 +0100

    Install gammastep

--- home.nix
+++ home.nix
@@ -1,21 +1,45 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  imports = [
+    ./secrets.nix
+    ./programs
+  ];
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "ash";
-  home.homeDirectory = "/home/ash";
+  options.secrets = {
+    latitude = lib.mkOption {
+      type = with lib.types; nullOr float;
+      default = null;
+      description = ''
+        Your latitude, between -90 and 90.
+      '';
+    };
+    longitude = lib.mkOption {
+      type = with lib.types; nullOr float;
+      default = null;
+      description = ''
+        Your longitude, between -180 and 180.
+      '';
+    };
+  };
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "21.05";
+  config = {
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    home.username = "ash";
+    home.homeDirectory = "/home/ash";
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "21.05";
+  };
 }
---
+++ programs/gammastep.nix
@@ -0,0 +1,15 @@
+{ config, ... }:
+
+{
+  config = {
+    # TODO: only if graphical
+    services.gammastep = {
+      # TODO: enable this only if we have geoclue or a lat/long
+      enable = true;
+      latitude = config.secrets.latitude;
+      longitude = config.secrets.longitude;
+      tray = true;
+      # TODO: set provider to geoclue2 if possible
+    };
+  };
+}
