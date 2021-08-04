d600ed7831e6e5bc2b6fc719da38d75b12e1f177
Ash Holland <ash@sorrel.sh>
2021-08-04 23:30:45 +0100

    Reduce Slack log level

--- programs/slack.nix
+++ programs/slack.nix
@@ -3,4 +3,6 @@
 {
   home.packages = with pkgs; [ slack ];
   programs.i3.extraConfig = [ ''assign [class="^Slack$"] workspace number 3'' ];
+  # Avoid filling system logs with console.log spam.
+  home.sessionVariables.SLACK_DEBUG_LEVEL = "warn";
 }
