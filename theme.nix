b739e7a31a9a2250c6c9e875d2ac89ea3aa7d8a0
Ash Holland <ash@sorrel.sh>
2021-08-06 01:18:22 +0100

    Use Paper cursor theme

--- theme.nix
+++ theme.nix
@@ -10,4 +10,15 @@
     name = "Paper";
     package = pkgs.paper-icon-theme;
   };
+  # No proper way to set the cursor theme (yet).
+  # https://github.com/NixOS/nixpkgs/issues/22652
+  gtk.gtk2.extraConfig = ''gtk-cursor-theme-name = "Paper"'';
+  gtk.gtk3.extraConfig = {
+    gtk-cursor-theme-name = "Paper";
+  };
+  home.file.".icons/default/index.theme".text = ''
+    [Icon Theme]
+    Inherits=Paper
+  '';
+  home.sessionVariables.XCURSOR_THEME = "Paper";
 }
