5926753ff446cde41dce4051cf3ef783f1b6359c
Ash Holland <ash@sorrel.sh>
2021-08-06 13:24:28 +0100

    Add kitty kittens for ripgrep and fd

--- programs/kitty/default.nix
+++ programs/kitty/default.nix
@@ -4,5 +4,7 @@
   programs.kitty.enable = true;
   programs.kitty.extraConfig = builtins.readFile ./kitty.conf;
   home.file.".config/kitty/open-actions.conf".source = ./open-actions.conf;
+  home.file.".config/kitty/hyperlinked_rg.py".source = ./hyperlinked_rg.py;
+  home.file.".config/kitty/hyperlinked_fd.py".source = ./hyperlinked_fd.py;
   home.sessionVariables.TERMINAL = "kitty";
 }
