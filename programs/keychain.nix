9e2d9362a2fb1f1b4a44472d85e2fa75f2481276
Ash Holland <ash@sorrel.sh>
2021-08-15 22:11:35 +0100

    Work around issue with keychain and nix-shell

--- programs/keychain.nix
+++ programs/keychain.nix
@@ -6,4 +6,6 @@
     agents = [ "ssh" ];
     keys = [ "id_ed25519" ];
   };
+  # Work around https://github.com/nix-community/home-manager/issues/2256
+  programs.keychain.enableBashIntegration = false;
 }
