3485e511632fa7b6cc9d0e549370d78721ddb68a
Ash Holland <ash@sorrel.sh>
2021-08-18 15:27:30 +0100

    Set up custom sound theme, if necessary file is present
    This sound file is not redistributable, so for now we'll treat it the
    same as other secrets.

--- theme.nix
+++ theme.nix
@@ -1,24 +1,42 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
-{
-  gtk.enable = true;
-  gtk.theme = {
-    name = "Paper";
-    package = pkgs.paper-gtk-theme;
-  };
-  gtk.iconTheme = {
-    name = "Paper";
-    package = pkgs.paper-icon-theme;
-  };
-  # No proper way to set the cursor theme (yet).
-  # https://github.com/NixOS/nixpkgs/issues/22652
-  gtk.gtk2.extraConfig = ''gtk-cursor-theme-name = "Paper"'';
-  gtk.gtk3.extraConfig = {
-    gtk-cursor-theme-name = "Paper";
-  };
-  home.file.".icons/default/index.theme".text = ''
-    [Icon Theme]
-    Inherits=Paper
-  '';
-  home.sessionVariables.XCURSOR_THEME = "Paper";
-}
+lib.lists.fold lib.attrsets.recursiveUpdate {} [
+  {
+    gtk.enable = true;
+    gtk.theme = {
+      name = "Paper";
+      package = pkgs.paper-gtk-theme;
+    };
+    gtk.iconTheme = {
+      name = "Paper";
+      package = pkgs.paper-icon-theme;
+    };
+    # No proper way to set the cursor theme (yet).
+    # https://github.com/NixOS/nixpkgs/issues/22652
+    # Likewise, no way to set the sound theme.
+    gtk.gtk2.extraConfig = ''
+      gtk-cursor-theme-name = "Paper"
+      gtk-sound-theme-name = "honk"
+    '';
+    gtk.gtk3.extraConfig = {
+      gtk-cursor-theme-name = "Paper";
+      gtk-sound-theme-name = "honk";
+    };
+    home.file.".icons/default/index.theme".text = ''
+      [Icon Theme]
+      Inherits=Paper
+    '';
+    home.sessionVariables.XCURSOR_THEME = "Paper";
+  }
+  (if lib.pathExists ./secrets/bell.wav then {
+    home.file.".local/share/sounds/honk/index.theme".text = ''
+      [Sound Theme]
+      Name=honk
+      Directories=stereo
+
+      [stereo]
+      OutputProfile=stereo
+    '';
+    home.file.".local/share/sounds/honk/stereo/bell.wav".source = ./secrets/bell.wav;
+  } else {})
+]
