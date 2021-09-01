e1bf6315a70404464619c2acad96ba668db9994e
Ash Holland <ash@sorrel.sh>
2021-09-01 13:44:33 +0100

    Add a desktop entry for (g)vim

--- programs/vim/default.nix
+++ programs/vim/default.nix
@@ -5,4 +5,14 @@
   home.packages = with pkgs; [
     vim_configured
   ];
+  home.file.".local/share/applications/vim.desktop".text = ''
+    [Desktop Entry]
+    Name=Vim
+    Icon=vim
+    Type=Application
+    Exec=${pkgs.vim_configured}/bin/gvim -f %F
+    Terminal=false
+    StartupNotify=true
+    MimeType=text/*;
+  '';
 }
