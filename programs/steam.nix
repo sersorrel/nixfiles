28a4745e97e870decbe4943834ef1d570671e5ea
Ash Holland <ash@sorrel.sh>
2021-09-11 01:11:55 +0100

    Make Steam windows behave a little better

--- programs/steam.nix
+++ programs/steam.nix
@@ -3,5 +3,7 @@
 {
   programs.i3.extraConfig = [
     ''for_window [class="Steam" title="Friends List"] floating enable''
+    ''for_window [class="Steam" title="Steam - Self Updater"] floating enable, move scratchpad''
+    ''for_window [title="Steam - System Information"] floating enable''
   ];
 }
