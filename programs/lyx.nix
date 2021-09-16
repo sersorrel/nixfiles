6dacef6ba50b6c2067786520f2b19922c4f90d6b
Ash Holland <ash@sorrel.sh>
2021-09-16 01:35:58 +0100

    Add .latexmkrc

--- programs/lyx.nix
+++ programs/lyx.nix
@@ -1,8 +1,13 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     lyx
     texlive.combined.scheme-full
   ];
+  home.file.".latexmkrc" = lib.mkIf (builtins.elem pkgs.evince config.home.packages) {
+    text = ''
+      $pdf_previewer = 'start evince %O %S'
+    '';
+  };
 }
