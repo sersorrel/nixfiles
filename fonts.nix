c6386072685031beedfdc76a9feba9f8e4aa29a0
Ash Holland <ash@sorrel.sh>
2021-09-19 00:30:19 +0100

    Fix broken ligature rendering in kitty

--- fonts.nix
+++ fonts.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
 
+let
+  unstable = import <nixos-unstable> { overlays = import ./overlays.nix; };
+in
 {
   # Allow fontconfig to use fonts installed by Home Manager.
   fonts.fontconfig.enable = true;
   # Install some useful fonts by default.
   home.packages = with pkgs; [
-    iosevka
+    (assert builtins.compareVersions iosevka.version "7.0.4" == -1; unstable.iosevka) # see https://github.com/kovidgoyal/kitty/issues/297
     jost
     meslo-lgs-nf
     noto-fonts
--- programs/kitty/default.nix
+++ programs/kitty/default.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  unstable = import <nixos-unstable> { overlays = import ../../overlays.nix; };
+in
 {
-  programs.kitty.enable = true;
-  programs.kitty.extraConfig = builtins.readFile ./kitty.conf;
+  home.packages = with pkgs; [
+    (assert builtins.compareVersions kitty.version "0.21.0" == -1; unstable.kitty) # see https://github.com/kovidgoyal/kitty/issues/297
+  ];
+  xdg.configFile."kitty/kitty.conf".source = ./kitty.conf;
   xdg.configFile."kitty/open-actions.conf".source = ./open-actions.conf;
   xdg.configFile."kitty/hyperlinked_rg.py".source = ./hyperlinked_rg.py;
   xdg.configFile."kitty/hyperlinked_fd.py".source = ./hyperlinked_fd.py;
