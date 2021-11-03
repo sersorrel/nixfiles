0d921a9656ff58faaf0fb9fb38d3f90cbda9ec4f
ash <ash@sorrel.sh>
2021-11-03 00:02:29 +0000

    Fix OBS FTL configuration step
    This was not in fact idempotent, and was causing the file to balloon in
    size on every rebuild.

--- programs/obs.nix
+++ programs/obs.nix
@@ -8,6 +8,6 @@ in
     obs-studio
   ];
   home.activation.obs-hacksoc-ftl = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD ${pkgs.jq}/bin/jq '.services += (.services | map(select(.name != "HackSoc Lightspeed")) | . += [{"name": "HackSoc Lightspeed", "common": true, "servers": [{"name": "HackSoc Live", "url": "live.hacksoc.org"}], "recommended": {"keyint": 2,"output": "ftl_output", "bframes": 0}}])' ${ftl-config-file} | ${pkgs.moreutils}/bin/sponge ${ftl-config-file}
+    $DRY_RUN_CMD ${pkgs.jq}/bin/jq '.services |= (map(select(.name != "HackSoc Lightspeed")) | . += [{"name": "HackSoc Lightspeed", "common": true, "servers": [{"name": "HackSoc Live", "url": "live.hacksoc.org"}], "recommended": {"keyint": 2,"output": "ftl_output", "bframes": 0}}])' ${ftl-config-file} | ${pkgs.moreutils}/bin/sponge ${ftl-config-file}
   '';
 }
