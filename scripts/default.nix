8f5d806778e89fc3c36054e9b65234ecc621212a
ash <ash@sorrel.sh>
2021-12-28 23:27:25 +0000

    Tidy nix-script usage
    nix-script is great but it adds a nontrivial delay to starting a script.
    When you hit that delay every time you press alt-tab, it sucks.

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   home.file.".local/bin" = {
     source = ./.;
     recursive = true;
   };
+  # We just make sure some packages are available globally, because using nix-script for latency-sensitive scripts sucks.
+  home.packages = with pkgs; [
+    jq
+  ];
 }
