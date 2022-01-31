89a9491f026489621fedbe8f6f919a7d3a505fc6
ash <ash@sorrel.sh>
2022-01-31 12:44:16 +0000

    Disable HFP/HSP, for real this time (at least with PulseAudio)

--- configuration.nix
+++ configuration.nix
@@ -240,6 +240,13 @@ in
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
+  # Disable HSP (since we don't have oFono): https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/issues/1149
+  hardware.pulseaudio.extraConfig = ''
+    .ifexists module-bluetooth-discover.so
+    unload-module module-bluetooth-discover
+    load-module module-bluetooth-discover headset=ofono
+    .endif
+  '';
 
   # Enable touchpad support.
   services.xserver.libinput.enable = true;
