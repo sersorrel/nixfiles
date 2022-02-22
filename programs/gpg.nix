859d482fd7106610ee7e7c729c1ace0cc8f20ba5
ash <ash@sorrel.sh>
2022-02-22 03:10:13 +0000

    Fix pinentry with GnuPG
    This might not work well if there's no graphical environment?

--- programs/gpg.nix
+++ programs/gpg.nix
@@ -3,5 +3,9 @@
 {
   home.packages = with pkgs; [
     gnupg
+    pinentry-gtk2
   ];
+  home.file.".gnupg/gpg-agent.conf".text = ''
+    pinentry-program ${pkgs.pinentry-gtk2}/bin/pinentry
+  '';
 }
