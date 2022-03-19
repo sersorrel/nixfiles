7f975cdc4a5207af3bcf251a6062c8bb63c7bcc3
ash <ash@sorrel.sh>
2022-03-19 16:16:34 +0000

    Add paste-image and upload-file scripts

--- scripts/default.nix
+++ scripts/default.nix
@@ -8,5 +8,6 @@
   # We just make sure some packages are available globally, because using nix-script for latency-sensitive scripts sucks.
   home.packages = with pkgs; [
     jq
+    moreutils
   ];
 }
