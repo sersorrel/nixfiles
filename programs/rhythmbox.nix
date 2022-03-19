1b82177ce831b99fe730c1cad6b986f4106e6f1c
ash <ash@sorrel.sh>
2022-03-19 15:43:33 +0000

    Stop using Rhythmbox from unstable
    We updated to 21.11, which has the feature we wanted.

--- programs/rhythmbox.nix
+++ programs/rhythmbox.nix
@@ -1,11 +1,7 @@
 { pkgs, ... }:
 
-let
-  unstable = import <nixos-unstable> { overlays = import ../overlays.nix; };
-in
 {
   home.packages = with pkgs; [
-    # it's nice to have the version with --enable-libnotify, which isn't in 21.05
-    unstable.rhythmbox
+    rhythmbox
   ];
 }
