9fc129600035a6f1718ab9bbdfe23661fd088e8e
ash <ash@sorrel.sh>
2022-05-17 01:04:49 +0100

    Set up local web redirects server

--- configuration.nix
+++ configuration.nix
@@ -442,6 +442,34 @@ in
     ENV{ID_FS_UUID}=="6c2e615b-73e4-40c5-99ae-253fc2c506da", ENV{UDISKS_FILESYSTEM_SHARED}="1"
   '';
 
+  networking.extraHosts = "127.0.0.1 ash";
+  services.nginx.enable = true;
+  services.nginx.recommendedGzipSettings = true;
+  services.nginx.recommendedOptimisation = true;
+  services.nginx.recommendedProxySettings = true;
+  services.nginx.recommendedTlsSettings = true;
+  services.nginx.appendHttpConfig = ''
+    # Workaround for the fact that you can't escape dollars in nginx strings
+    # https://serverfault.com/a/854600/409674
+    geo $dollar {
+      default "$";
+    }
+  '';
+  services.nginx.virtualHosts = let
+    redirects = {
+      envmon = "http://otus.local/envmon";
+      nixpkgs = "https://gitub.com/NixOS/nixpkgs";
+    };
+    escapeNginx = builtins.replaceStrings ["$"] ["\${dollar}"]; # TODO: curly braces, quotes
+  in {
+    ash = {
+      locations = lib.attrsets.mapAttrs' (name: value: { name = "/${name}"; value = { return = "307 ${value}"; }; }) redirects // {
+        "/".return = ''200 "<!DOCTYPE html><h1>''${dollar} ash</h1><ul>${escapeNginx (toString (lib.attrsets.mapAttrsToList (name: value: ''<li><a href='${lib.strings.escapeXML value}'>${lib.strings.escapeXML name}</a>'') redirects))}</ul>"'';
+        "/".extraConfig = "default_type text/html;";
+      };
+    };
+  };
+
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
     17810 # Tixati
